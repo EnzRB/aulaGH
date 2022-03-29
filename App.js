3b8357de2e780aa043648591e7c8f822f4cf3fb4
Your Name <you@example.com>
2022-03-29 10:39:53 -0300

    flatlist

--- App.js
+++ App.js
@@ -2,6 +2,8 @@
 import { StyleSheet, Text, View } from "react-native";
 import Cabecalho from "./src/components/cabecalho";
 import Botao from "./src/components/botao";
+import Jogos from "./src/components/jogos";
+import Dados from "./dados/dadosJogos.js"
 
 
 export default function App() {
@@ -14,7 +16,24 @@ export default function App() {
       Logo2 ="musical-note" texto2="Musicas" cor2="red" ></Botao>
       <Botao Logo="book-sharp" texto="Livros" cor="orange"
       Logo2 ="newspaper-outline" texto2="Noticias" cor2="blue" ></Botao>
-     
+     <View>
+       <FlatList
+       horizontal={true}
+       data = {Dados}
+       keyExtractor = {(item) => item.id}
+       renderItem = { ({ item }) => (
+
+        <Jogos
+        titulo ={item.nome}
+        imagem = {item.imagem}
+        valor = {item.valor}
+        />
+       )}
+
+       />
      </View>
+     </View>
+
+     
   );
 }
---
+++ dados/dadosJogos.js
@@ -0,0 +1,32 @@
+const JOGOS = [
+    {
+        id : "01",
+        nome : "Subway Surfers",
+        valor : 13.32,
+        imagem : 'subway.jpg'
+    },
+    {
+        id : "02",
+        nome : "Yugioh Master Duel",
+        valor : "gratuito",
+        imagem : "yugi.jpg"
+    },
+    {
+        id : "03",
+        nome : "Pokemon Unite",
+        valor : "gratuito",
+        imagem : "pokemon.jpg"
+    },
+    {
+        id : "04",
+        nome : "Genshin Impact",
+        valor : "gratuito",
+        imagem : "genshin.png"
+    },
+    {
+        id : "05",
+        nome : "Dragon Ball Z Dokkan Battle",
+        valor : "gratuito",
+        imagem : "dbz.png"
+    },
+]
